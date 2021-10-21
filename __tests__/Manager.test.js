9bc3219255fbef3ca36ae6012e5d97fd004a73e9
Paige A <pvaltemare@gmail.com>
2021-10-20 17:15:46 -0700

    Manager object

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,17 @@
-const Manager = require('../lib/Manger.js');
+const Manager = require('../lib/Manager.js');
 
+test('creates an manager object', () => {
+    const manager = new Manager('Dave', '10', 'dave@gmail.com', '125');
+
+    expect(manager.name).toBe('Dave');
+    expect(manager.id).toBe('10');
+    expect(manager.email).toBe('dave@gmail.com');
+    expect(manager.officeNumber).toBe('125');
+});
+
+// getRole() // Returns 'Manager'
+test("gets manager's role", () => {
+    const manager = new Manager('Dave', '10', 'dave@gmail.com', '125');
+
+    expect(manager.getRole()).toEqual(expect.stringContaining('Manager'));
+});
